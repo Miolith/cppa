cbe98136c3ac6addff2bfdd48ba33340911882cd
Nam Duong <nam.duong@epita.fr>
2021-05-15 18:34:11 +0200

    Add a dummy test for dilate1d firstVersion

--- src/firstVersion.hpp
+++ src/firstVersion.hpp
@@ -5,10 +5,10 @@
  * \brief First version template
  * Dilation by a centered horizontal structuring element of length 2*k + 1.
  *
- * \param in Input image 2D
- * \param out Computed image 2D
+ * \param in<T> Input image 2D
+ * \param out<T> Computed image 2D
  * \param k Radius  of the sliding windows
- * \param sup Supremum operator
+ * \param sup Supremo operator
  */
 template<class T, class BinaryOperation>
 void dilate1d(image2d<T> in, image2d<T> out, int k, BinaryOperation sup, T zero)
@@ -21,8 +21,8 @@ void dilate1d(image2d<T> in, image2d<T> out, int k, BinaryOperation sup, T zero)
     const int alpha = 2 * k + 1;
 
     // Temporary images to store results
-    auto g = new image2d<T>;
-    auto h = new image2d<T>;
+    image2d<T> g(in.width(), in.height());
+    image2d<T> h(in.width(), in.height());
 
     // Forward pass
     // Compute g[x] = Max f(y), y ∈ [α * ⌊x / α⌋ : x]
@@ -34,12 +34,12 @@ void dilate1d(image2d<T> in, image2d<T> out, int k, BinaryOperation sup, T zero)
         {
             int chunk_size = std::min(rem, alpha);
             std::partial_sum(
-                    (in)(chunk_start, zero),
-                    (in)(chunk_start, chunk_size),
-                    (g)(chunk_start, zero),
+                    in(chunk_start, zero),
+                    in(chunk_start, chunk_size),
+                    g(chunk_start, zero),
                     /* /!\ They may use the pointer arithmetic
                      * for those parameters in the implementation
-                     * from the subjet.
+                     * from the subject.
                      */
                     sup);
         }
@@ -54,23 +54,23 @@ void dilate1d(image2d<T> in, image2d<T> out, int k, BinaryOperation sup, T zero)
         {
             int chunk_size = std::min(alpha, rem);
             std::partial_sum(
-                    std::make_reverse_iterator((in)(chunk_start, chunk_size)),
-                    std::make_reverse_iterator((in)(chunk_start, zero)),
-                    std::make_reverse_iterator((h)(chunk_start, chunk_size)),
+                    in(chunk_start, chunk_size), // /!\ RIP
+                    in(chunk_start, zero),
+                    h(chunk_start, chunk_size),
                     /* /!\ They may use the pointer arithmetic
                      * for those parameters in the implementation
-                     * from the subjet.
+                     * from the subject.
                      */
                     sup);
         }
     }
     // Compute local maximum out[x] = Max f(y) y ∈ [x-k:x+k]
-    // out[x] = Max   (Max f[x-k:b),  Max f[b:x+k]) with b = α.⌈(x-k)/α⌉ = α.⌊(x+k)/α⌋
+    // out[x] = Max   (Max f[x-k:b],  Max f[b:x+k]) with b = α.⌈(x-k)/α⌉ = α.⌊(x+k)/α⌋
     //        = Max( h[x-k], g[x+k] )
     {
         for (int i = 0; i < n; i++)
         {
-            out.m_buffer[i] = sup((h)(i-k, zero), (g)(i+k, zero));
+            out(i, zero) = sup(h(i-k, zero), g(i+k, zero));
         }
     }
 }
--- tests/test_dummy.cpp
+++ tests/test_dummy.cpp
@@ -1,23 +1,40 @@
 #include <cppa/image.hpp>
 
 #include <gtest/gtest.h>
+#include "../src/firstVersion.hpp"
 
 
-image2d<int> generate_iota(int w, int h)
-{
-  image2d<int> f(w, h);
+image2d<int> generate_iota(int w, int h) {
+    image2d<int> f(w, h);
 
+    for (int y = 0; y < h; ++y)
+        for (int x = 0; x < w; ++x)
+            f(x, y) = w * y + x;
 
-  for (int y = 0; y < h; ++y)
-    for (int x = 0; x < w; ++x)
-      f(x, y) = w * y + x;
-
-  return f;
+    return f;
+}
+void print(image2d<int> in) {
+    for (int i = 0; i < in.height(); ++i) {
+        for (int j = 0; j < in.width(); ++j) {
+            std::cout << in(i, j) << ' ';
+        }
+        std::cout << std::endl;
+    }
 }
 
+int max(int a, int b) {
+    return a < b ? b : a;
+}
 
-TEST(CPPA, test_dummy)
-{
-  auto f = generate_iota(10,10);
-  ASSERT_EQ(f(5,5), 55);
+TEST(CPPA, test_dummy) {
+    auto f = generate_iota(10, 10);
+    ASSERT_EQ(f(5, 5), 55);
+}
+
+TEST(CPPA, dummy1) {
+    auto in = generate_iota(5, 5);
+    image2d<int> out(5, 5);
+    dilate1d(in, out, 1, max, 0);
+    print(in);
+    print(out);
 }
